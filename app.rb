6fd789f5dceeae317aba63e4bf9f03e8a82e0e9b
Justin Tooley <jstooley823@gmail.com>
2017-11-13 22:55:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,13 +5,14 @@ class App < Sinatra::Base
     enable :sessions
     set :session_secret, "secret"
   end
+  
   get '/' do
     erb :index
   end
 
   post "/checkout" do
     @item = params['item']
-  
+
     erb :checkout
   end
 
