d18030f00c955ffced4b2628cb1c72eace59b5a5
Justin Tooley <jstooley823@gmail.com>
2017-11-13 23:11:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   post "/checkout" do
     @item = params['item']
     binding.pry
-    @session = sesson
+    @session = session
   
     @session= { item:  @item['name']}
 
